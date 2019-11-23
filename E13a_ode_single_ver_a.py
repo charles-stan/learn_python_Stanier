3ea4d420fac6165f9564355ded5425d5a10fa508
Charles Stanier <charles.stanier@gmail.com>
2019-11-23 11:48:38 -0600

    Add files via upload
    updates to E13a and E13b

--- E13a_ode_single_ver_a.py
+++ E13a_ode_single_ver_a.py
@@ -17,11 +17,16 @@ as we may see in controls.
 however, if you search the internet, you may find odeint
 examples that will look slightly different
 
-Log of changes: none so far
+Log of changes: 
+    Nov 23, 2019
+    added a flag so that this will use solve_ivp or odeint
+    please note that the derivative function for solve_ivp must be
+    deriv(t,y) and for odeint it is deriv(y,t)
 """
 
 import numpy as np
 from scipy.integrate import solve_ivp
+from scipy.integrate import odeint
 import pylab as pl
 
 # Variables
@@ -31,7 +36,18 @@ import pylab as pl
 # k = outflow coef
 # equation dh/dt = Fin/A - k/A x sqrt(h)
 
-def deriv_func( t, h):
+def deriv_func_tfirst( t, h):
+    k = 0.1
+    A = 0.2
+    if t>50:
+        Fin = 2
+    else:
+        Fin = 0
+    dhdt = Fin/A - k/A*np.sqrt(h)
+    print('deriv called at t = ',t,' h= ',h, ' deriv = ',dhdt)
+    return dhdt
+
+def deriv_func_hfirst( h, t):
     k = 0.1
     A = 0.2
     if t>50:
@@ -46,17 +62,30 @@ def deriv_func( t, h):
 # sets up the initial conditsions, calls the ode integrator,
 # and plots the solution
     
+odeint_flag = False # set to True to use odeint, set to False to use solve_ivp
+    
 # initial condition
 hinit = 0
 teval = np.linspace(0,800)
 
-soltn = solve_ivp( deriv_func, [0, 800], [hinit] )
-# solve_ivp(fun, t_span, y0, 
-#   method='RK45', t_eval=None, dense_output=False, 
-#   events=None, vectorized=False, **options)
 
-t_plot = soltn.t # time values to plot
-h_plot = soltn.y[0,:] # heights to plot
+if odeint_flag == False: # use solve_ivp
+    soltn = solve_ivp( deriv_func_tfirst, [0, 800], [hinit] )
+    # solve_ivp(fun, t_span, y0, 
+    #   method='RK45', t_eval=None, dense_output=False, 
+    #   events=None, vectorized=False, **options)
+    t_plot = soltn.t # time values to plot
+    h_plot = soltn.y[0,:] # heights to plot
+    
+if odeint_flag == True: # use odeint
+    t_plot = np.linspace(0,800,800)    
+    soltn = odeint( deriv_func_hfirst, [hinit], t_plot )
+    # scipy.integrate.odeint(func, y0, t, args=(), Dfun=None, col_deriv=0, 
+    #   full_output=0, ml=None, mu=None, rtol=None, atol=None, tcrit=None, h0=0.0, 
+    #   hmax=0.0, hmin=0.0, ixpr=0, mxstep=0, mxhnil=0, mxordn=12, mxords=5, printmessg=0, 
+    #   tfirst=False)[source]
+    h_plot = soltn
+
 # [:,1] forced the matrix to an array
 
 # plot the results
--- E13b_ode_single_ver_b.py
+++ E13b_ode_single_ver_b.py
@@ -11,8 +11,8 @@ derivative function and driver in same file
 defining problem parameters as a dictionary
 in the driver and passing that to the derivative 
 scipy.integrate.solve_ivp does not permit passing of 
-additional arguments, so we use the awkward lambda 
-construction per https://github.com/scipy/scipy/issues/8352
+additional arguments, so we use the lambda 
+construction 
 
 this will be fixed with scipy version 1.4 but it has not 
 yet been released
@@ -24,7 +24,36 @@ and keep all of your model specifications in one place.
 for engineering problems, you can add notes and/or units to the pinfo dictionary
 as well as error codes.  This can keep you organized and minimize chance of mistakes
 
-Log of changes:  none so far
+Log of changes:  
+    Nov 23, 2019   Tested whether new version of scipy that did not require
+    lambda functions was available (it is not).  Explained lambda functions better in a comment
+"""
+
+"""
+lambda function use
+we are using one line with "lambda" in it to get around a problem with solve_ivp
+for neat, readable code, we want to pass parameters to the derivative function
+ideally, this would be done through
+   scipy.solve_my_ode_python( fun=deriv_fun, tvec, yo, pinfo )  similar to what is done in
+   matlab or in python's scipy.odeint.  However, odeint has not handled the sharp change in the forcing
+   function in my tests.  So, hence we are using the lambda function and scipy.solve_ivp
+   
+   lamba is an anonymous function (no name) that is declared and used right at the point of declaration
+   there is no def function code
+   see 
+   per https://github.com/scipy/scipy/issues/8352
+   and lamba functions are better explained at https://www.afternerd.com/blog/python-lambdas/
+   
+   the lambda function syntax is 
+   lambda input_args : expression
+   
+   normally, you would call solve_ivp with solve_ivp( fun=deriv_fun, t, yo ) but there is no way to 
+   pass the pinfo dictionary.  So we replace fun=deriv_fun with the lambda function
+   
+   solve_ivp( fun=lambda t,h: deriv_func(t,h,pinfo), t, yo )
+   
+   solve ivp will pass the 'normal' input arguments (t and h) to the lambda function,
+   and the lambda function will pass t, h and pinfo to the derivative function
 
 """
 
